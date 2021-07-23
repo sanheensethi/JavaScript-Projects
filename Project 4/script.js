cff359b98bce3a2737b211b6002a861dd1c12545
Sanheen Sethi <sanheensethi37659@gmail.com>
2021-07-24 02:17:43 +0530

    4.0.2

--- Project 4/script.js
+++ Project 4/script.js
@@ -6,7 +6,6 @@ apiData = [
 ];
 country = `in`;
 baseURL = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiData[0].api}`;
-// baseURL = "data.json";
 let news = document.getElementById('news');
 let mainImg = document.getElementById('mainImg');
 let maini = document.getElementById('maini');
@@ -14,10 +13,8 @@ let newsExtra = document.getElementById('newsExtra');
 let spinner = document.getElementById('spinner');
 let content = document.getElementById('content');
 let search = document.getElementById('search');
-let flag = 0;
 search.addEventListener('click',function(e){
 	e.preventDefault();
-	flag = 1;
 	searchkro();
 });
 
@@ -95,6 +92,9 @@ xhr.onload = function(){
  		newsExtra.innerHTML = html3;
  		spinner.style.display = 'none';
  		content.style.display = 'block';
+ 	}else{
+ 		alert(data.code);
+ 		alert(data.messgae);
  	}
  }
 
@@ -109,114 +109,88 @@ function searchkro(){
 	spinner.style.display = 'block';
 	news.innerHTML = '';
 	let baseURL2 = `https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=${apiData[0].api}`;
-	console.log(baseURL2);
 	let xhr = new XMLHttpRequest();
     xhr.open('GET',baseURL2,true);
 
     xhr.onload = function(){
- 	let data = JSON.parse(xhr.responseText);
- 	if(data.status == "ok"){
- 		console.log(data);
- 		let html = ``;
- 		mainImg.innerHTML = '';
- 		mainImg.innerHTML = `<div class="carousel-item active">
-                				<img id="maini" class="d-block w-100 filt" alt="Image">
-              				 </div>`;
-        newsExtra.innerHTML = ``;
- 		let html2 = mainImg.innerHTML;
- 		let html3 = newsExtra.innerHTML;
- 		let articles = data.articles;
- 		let images = [];
- 		articles.forEach(function(element,index){
- 			if(element.urlToImage!=null){
- 				images.push(element.urlToImage);
- 			}
- 		});
- 		articles.forEach(function(element,index){
- 			if(element.urlToImage!=null && element.content!=null){
- 				maini.src = images[Math.floor(Math.random()*images.length)];
- 				text = element.content;
-				if(element.content == ""){ text = element.description; }
-				let datestring = element.publishedAt;
-				let inx = datestring.indexOf('T');
-				let date = datestring.substring(0,inx);
-				let time = datestring.substring(inx+1,datestring.length-1); 
- 				html += `<div class="col">
-					             <div class="card my-2" style="width: 18rem;">
-					              <img src="${element.urlToImage}" class="card-img-top" alt="Image">
-					              <div class="card-body">
-					                <h5 class="card-title">${element.title}</h5>
-					                <p class="card-text">${text}</p>
-					                <a href="${element.url}" class="btn btn-link">Read More</a>
-					                <h5>Source: <span class="badge bg-secondary">${element.source.name}</span></h5>
-					                <p>Published At: ${date}<br>Time: ${time}</p>
-					              </div>
-					            </div>
-					        </div>`;
-				html2 += ` <div class="carousel-item">
-                				<img src="${element.urlToImage}" class="d-block w-100 filt" alt="Image">
-                				<div class="carousel-caption d-none d-md-block" style="background-color:rgba(60, 60, 60, 0.6);padding:5px;box-shadow:2px 3px 5px #000;">
-        							<h5>${element.title}</h5>
-        							<p>Rapid News</p>
-      							</div>
-             			   </div>`;
- 			}else{
- 				let text = "";
- 				if(element.description == null || element.description == ""){
- 					if(element.content == "" || element.content == null) text = "";
- 					else text = element.content;
- 				} 
- 				html3 += `
- 					<div class="accordion-item">
-		              <h2 class="accordion-header" id="flush-heading${index}">
-		                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapse${index}">
-		                  ${element.title}
-		                </button>
-		              </h2>
-		              <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}" data-bs-parent="#newsExtra">
-		                <div class="accordion-body">${text} <a href="${element.url}" class="btn btn-link">Read Here</a>
-					                <h4>Source: <span class="badge bg-secondary">${element.source.name}</span></h4></div>
-					                <p>Published At: ${element.publishedAt}</p>
-		              </div>
-		            </div>
- 				`;
- 			}
- 		});
- 		news.innerHTML = html;
- 		mainImg.innerHTML = html2;
- 		newsExtra.innerHTML = html3;
- 		spinner.style.display = 'none';
- 		content.style.display = 'block';
+	 	let data = JSON.parse(xhr.responseText);
+	 	if(data.status == "ok"){
+	 		console.log(data);
+	 		let html = ``;
+	 		mainImg.innerHTML = '';
+	 		mainImg.innerHTML = `<div class="carousel-item active">
+	                				<img id="maini" class="d-block w-100 filt" alt="Image">
+	              				 </div>`;
+	        newsExtra.innerHTML = ``;
+	 		let html2 = mainImg.innerHTML;
+	 		let html3 = newsExtra.innerHTML;
+	 		let articles = data.articles;
+	 		let images = [];
+	 		articles.forEach(function(element,index){
+	 			if(element.urlToImage!=null){
+	 				images.push(element.urlToImage);
+	 			}
+	 		});
+	 		articles.forEach(function(element,index){
+	 			if(element.urlToImage!=null && element.content!=null){
+	 				maini.src = images[Math.floor(Math.random()*images.length)];
+	 				text = element.content;
+					if(element.content == ""){ text = element.description; }
+					let datestring = element.publishedAt;
+					let inx = datestring.indexOf('T');
+					let date = datestring.substring(0,inx);
+					let time = datestring.substring(inx+1,datestring.length-1); 
+	 				html += `<div class="col">
+						             <div class="card my-2" style="width: 18rem;">
+						              <img src="${element.urlToImage}" class="card-img-top" alt="Image">
+						              <div class="card-body">
+						                <h5 class="card-title">${element.title}</h5>
+						                <p class="card-text">${text}</p>
+						                <a href="${element.url}" class="btn btn-link">Read More</a>
+						                <h5>Source: <span class="badge bg-secondary">${element.source.name}</span></h5>
+						                <p>Published At: ${date}<br>Time: ${time}</p>
+						              </div>
+						            </div>
+						        </div>`;
+					html2 += ` <div class="carousel-item">
+	                				<img src="${element.urlToImage}" class="d-block w-100 filt" alt="Image">
+	                				<div class="carousel-caption d-none d-md-block" style="background-color:rgba(60, 60, 60, 0.6);padding:5px;box-shadow:2px 3px 5px #000;">
+	        							<h5>${element.title}</h5>
+	        							<p>Rapid News</p>
+	      							</div>
+	             			   </div>`;
+	 			}else{
+	 				let text = "";
+	 				if(element.description == null || element.description == ""){
+	 					if(element.content == "" || element.content == null) text = "";
+	 					else text = element.content;
+	 				} 
+	 				html3 += `
+	 					<div class="accordion-item">
+			              <h2 class="accordion-header" id="flush-heading${index}">
+			                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapse${index}">
+			                  ${element.title}
+			                </button>
+			              </h2>
+			              <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}" data-bs-parent="#newsExtra">
+			                <div class="accordion-body">${text} <a href="${element.url}" class="btn btn-link">Read Here</a>
+						                <h4>Source: <span class="badge bg-secondary">${element.source.name}</span></h4></div>
+						                <p>Published At: ${element.publishedAt}</p>
+			              </div>
+			            </div>
+	 				`;
+	 			}
+	 		});
+	 		news.innerHTML = html;
+	 		mainImg.innerHTML = html2;
+	 		newsExtra.innerHTML = html3;
+	 		spinner.style.display = 'none';
+	 		content.style.display = 'block';
+	 	}else{
+ 			alert(data.code);
+ 			alert(data.messgae);
+ 		}
  	}
- }
-
-xhr.send();
-
+	xhr.send();
 }
-
 body();
-
-/*
-
-author: null
-content: "Bach thanks athletes for their commitment and enthusiasm!\r\nWhat is true for the perseverance of the Japanese people is also true for you, my fellow Olympic athletes. \r\nYou had to face great challenge… [+922 chars]"
-description: "Tokyo Olympics Opening Ceremony LIVE: Check all the live updates of Olympics Opening Ceremony’s, Tokyo Olympics 2020 opening ceremony is scheduled to begin at 4:30 pm IST on 23 July, Olympics Latest News Updates at Firstpost"
-publishedAt: "2021-07-23T14:21:03Z"
-source: {id: null, name: "Firstpost"}
-title: "Tokyo Olympics 2020 Opening Ceremony Live: Japanese emperor declares the Games open - Firstpost"
-url: "https://www.firstpost.com/sports/tokyo-olympics-2020-opening-ceremony-live-updates-9828691.html"
-urlToImage: "https://images.firstpost.com/wp-content/uploads/2021/07/AP21204431011827_opt.jpg"
-__proto__: Object
-
-*/
-
-
-
-
-
-
-
-
-
-
-
